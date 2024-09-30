055830740c05da29f7fff69d4811f5c4dbea1a79
linxs953 <cakelinxs@outlook.com>
2024-09-30 17:09:16 +0800

    refactor: 任务运行详情组件store

---
+++ src/store/task/taskDetail.ts
@@ -0,0 +1,98 @@
+
+import { StoreApi, UseBoundStore } from "zustand"
+import createSelectors from "../selectors"
+import createStore from "../store"
+
+interface TaskRunDetailState {
+    taskRecord: TaskRunRecord
+    setTaskRecord: (taskRecord: TaskRunRecord) => void
+    
+    sceneRecords: SceneRecord[]
+    setSceneRecords: (sceneRecords: SceneRecord[]) => void
+    
+    loading: boolean
+    setLoading: (loading: boolean) => void
+    
+    isActionModalVisible: boolean
+    setIsActionModalVisible: (isActionModalVisible: boolean) => void
+    
+    selectedAction: ActionRecord | null
+    setSelectedAction: (selectedAction: ActionRecord | null) => void
+    
+    isSceneModalVisible: boolean
+    setIsSceneModalVisible: (isModalVisible: boolean) => void
+
+    selectedScene: SceneRecord | null
+    setSelectedScene: (selectedScene: SceneRecord | null) => void
+    
+}
+
+const createTaskRunDetailStore = (): UseBoundStore<StoreApi<TaskRunDetailState>> => {
+    const initialState = {
+        taskRecord: {} as TaskRunRecord,
+        
+        sceneRecords: [],
+
+        loading: false,
+
+        isActionModalVisible: false,
+        
+        selectedAction: {} as ActionRecord,
+
+        isSceneModalVisible: false,
+
+        selectedScene: {} as SceneRecord,
+    }
+    const actions = (set:any) => ({
+        setTaskRecord: (taskRecord: TaskRunRecord) => {
+            set((state:any) => ({
+                taskRecord: taskRecord
+            }))
+        },
+        
+        setSceneRecords: (sceneRecords: SceneRecord[]) => {
+            set((state:any) => ({
+                sceneRecords: sceneRecords
+            }))
+        },
+        
+        setLoading: (loading: boolean) => {
+            set((state:any) => ({
+                loading: loading
+            }))
+        },
+        
+        setIsActionModalVisible: (isActionModalVisible: boolean) => {
+            set((state:any) => ({
+                isActionModalVisible: isActionModalVisible
+            }))
+        },
+        
+        
+        setSelectedAction: (selectedAction: ActionRecord) => {
+            set((state:any) => ({
+                selectedAction: selectedAction
+            }))
+        },
+
+        setIsSceneModalVisible: (isSceneModalVisible: boolean) => {
+            set((state:any) => ({
+                isSceneModalVisible: isSceneModalVisible
+            }))
+        },
+
+        setSelectedScene: (selectedScene: SceneRecord) => {
+            set((state:any) => ({
+                selectedScene: selectedScene
+            }))
+        },
+        
+    })
+
+    const useNewTaskModalStore:any = createStore(initialState,actions)
+
+    return createSelectors(useNewTaskModalStore)
+}
+
+
+export default createTaskRunDetailStore
--- src/store/task/types.ts
+++ src/store/task/types.ts
@@ -1,6 +1,6 @@
 
 
-
+// 创建任务Modal
 interface addSceneData {
     id: number
     text: string
@@ -10,5 +10,43 @@ interface addSceneData {
 interface searchSceneItem {
     id: number
     text: string
+}
+
+
+
+// 任务运行详情
+interface SceneRecord {
+    sceneId: string;
+    sceneName: string;
+    duration: number;
+    total: number;
+    success: number;
+    fail: number;
+    finish: number;
+    status: number;
+    error: Object;
+    actionRecords: ActionRecord[];
+}
+
+
+interface ActionRecord {
+    actionId: string;
+    actionName: string;
+    request: Object;
+    response: Object;
+    duration: number;
+    status: string;
+    error: Object;
+}
+
+interface TaskRunRecord {
+  taskId: string;
+  taskRunId: string;
+  taskName: string;
+  author: string
+  createAt: string
+  updateAt: string
+  scenesRecords: SceneRecord[];
+  status: number;
 }
 
